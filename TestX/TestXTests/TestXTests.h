8dcbca41b9ac415e4132764a520f4d69b6745ab8
Chuya Miura <hoshininegaiwo2poul@yahoo.co.jp>
2012-08-26 01:25:07 +0900

    test

---
+++ TestX/TestXTests/TestXTests.h
@@ -0,0 +1,13 @@
+//
+//  TestXTests.h
+//  TestXTests
+//
+//  Created by 宙也 三浦 on 12/08/25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestXTests : SenTestCase
+
+@end
---
+++ TestX/TestXTests/TestXTests.m
@@ -0,0 +1,32 @@
+//
+//  TestXTests.m
+//  TestXTests
+//
+//  Created by 宙也 三浦 on 12/08/25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestXTests.h"
+
+@implementation TestXTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestXTests");
+}
+
+@end
